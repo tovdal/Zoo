f553edfd192d7361381a3667b1924bb8c871549b
Tova Dalin <tova.dalin@hotmail.com>
2025-01-22 14:42:25 +0100

    Removed because it is not suppose to be in the assignment

--- scripts.js
+++ scripts.js
@@ -1,32 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav ul li a');
-    const offset = 100; // Adjust this value to set the offset
+// document.addEventListener('DOMContentLoaded', function() {
+//     const links = document.querySelectorAll('nav ul li a');
+//     const offset = 100; // Adjust this value to set the offset
 
-    for (const link of links) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+//     for (const link of links) {
+//         link.addEventListener('click', function(event) {
+//             event.preventDefault();
+//             const targetId = this.getAttribute('href').substring(1);
+//             const targetElement = document.getElementById(targetId);
 
-            window.scrollTo({
-                top: targetElement.offsetTop - offset,
-                behavior: 'smooth'
-            });
-        });
-    }
-});
-document.addEventListener('DOMContentLoaded', function() {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-    const navLinks = document.querySelectorAll('.nav-menu li a');
-
-    navToggle.addEventListener('click', function() {
-      navMenu.classList.toggle('show');
-    });
-
-    navLinks.forEach(link => {
-      link.addEventListener('click', function() {
-        navMenu.classList.remove('show');
-      });
-    });
-  });
+//             window.scrollTo({
+//                 top: targetElement.offsetTop - offset,
+//                 behavior: 'smooth'
+//             });
+//         });
+//     }
+// });
